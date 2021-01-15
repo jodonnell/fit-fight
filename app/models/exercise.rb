5e73cb98e15bf7a63017c1b6a92172926380b8c3
Steven Nunez <octosteve@github.com>
2021-01-15 15:18:12 -0500

    Update exercise.rb

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -3,11 +3,6 @@ class Exercise < ApplicationRecord
   has_many :daily_exercise, through: :exercise_counts
 
   def self.empty_hash
-    exercises = all
-    result = {}
-    exercises.each do |exercise|
-      result[exercise.name] = 0
-    end
-    result
+    pluck(:name).zip([0].cycle).to_h
   end
 end
