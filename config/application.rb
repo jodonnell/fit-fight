c0c68d25ea69f4219ea7c1d14426248e0fbb4fe4
Jacob O'Donnell <jacobodonnell@jacobs-laptop.myfiosgateway.com>
2024-07-18 20:27:59 -0400

    set server time zone to est

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module FitFight
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Eastern Time (US & Canada)'
+
   end
 end
